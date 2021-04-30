7a8e81134090f87c97b3923d721aa7381632c4cf
Aya1602 <aygerim.m1602@gmail.com>
2021-04-30 08:18:23 +0600

    Initial commit

---
+++ app/src/main/java/com/example/snackbar/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public  void onClick(View view){
+        Snackbar snackbar = Snackbar.make(view, "Hello Android", Snackbar.LENGTH_LONG);
+
+        snackbar.setAction("Next...", new View.OnClickListener (){
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Next clicked!",Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+        snackbar.show();
+    }
+}
